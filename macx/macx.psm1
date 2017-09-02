c17475d9134c584e33133aeca925a42893cb33da
mcurole <30864938+mcurole@users.noreply.github.com>
2017-09-02 17:38:00 -0400

    Fixed manifest and removed unused commands

--- macx/macx.psm1
+++ macx/macx.psm1
@@ -1,99 +1,4 @@
-Function Export-Xls {
-    <#
-.SYNOPSIS
-	Saves Microsoft .NET Framework objects to a worksheet in an XLS file
-.DESCRIPTION
-	The Export-Xls function allows you to save Microsoft .NET Framework objects
-	to a named worksheet in an Excel file (type XLS). The position of the
-	worksheet can be specified.
-.NOTES
-	Author:  Luc Dekens
-.PARAMETER InputObject
-	Specifies the objects to be written to the worksheet. The parameter accepts
-	objects through the pipeline.
-.PARAMETER Path
-	Specifies the path to the XLS file.
-.PARAMETER WorksheetName
-	The name for the new worksheet. If not specified the name will
-	be "Sheet" followed by the "Ticks" value
-.PARAMETER SheetPosition
-	Specifies where the new worksheet will be inserted in the series of
-	existing worksheets. You can specify "begin" or "end". The default
-	is "begin".
-.PARAMETER NoTypeInformation
-	Omits the type information from the worksheet. The default is to
-	include the "#TYPE" line.
-.PARAMETER AppendWorksheet
-	Specifies if the worksheet should keep or remove the existing
-	worksheet in the spreadsheet. The default is to append.
-.EXAMPLE
-	PS> Export-Xls -
-#>
-    param(
-        [parameter(ValueFromPipeline = $true, Position = 1)]
-        [ValidateNotNullOrEmpty()]
-        $InputObject,
-        [parameter(Position = 2)]
-        [ValidateNotNullOrEmpty()]
-        [string]$Path,
-        [string]$WorksheetName = ("Sheet " + (Get-Date).Ticks),
-        [string]$SheetPosition = "begin",
-        [switch]$NoTypeInformation = $true,
-        [switch]$AppendWorksheet = $true
-    )
-
-    Begin {
-        $excelApp = New-Object -ComObject "Excel.Application"
-        $originalAlerts = $excelApp.DisplayAlerts
-        $excelApp.DisplayAlerts = $false
-        $excelApp.Visible = $true
-        if (Test-Path -Path $Path -PathType "Leaf") {
-            $workBook = $excelApp.Workbooks.Open($Path)
-        }
-        else {
-            $workBook = $excelApp.Workbooks.Add()
-        }
-        $sheet = $excelApp.Worksheets.Add($workBook.Worksheets.Item(1))
-        if (!$AppendWorksheet) {
-            $workBook.Sheets | Where-Object {$_ -ne $sheet} | ForEach-Object {$_.Delete()}
-        }
-        $sheet.Name = $WorksheetName
-        if ($SheetPosition -eq "end") {
-            $nrSheets = $workBook.Sheets.Count
-            2..($nrSheets) | ForEach-Object {
-                $workbook.Sheets.Item($_).Move($workbook.Sheets.Item($_ - 1))
-            }
-        }
-        $tempCsvName = $env:Temp + "\Report-" + (Get-Date).Ticks + ".csv"
-        $array = @()
-    }
-
-    Process {
-        $array += $InputObject
-    }
-
-    End {
-        $array | Export-Csv -Path $tempCsvName -NoTypeInformation:$NoTypeInformation
-        $csvBook = $excelApp.Workbooks.Open($tempCsvname)
-        $csvSheet = $csvBook.Worksheets.Item(1)
-        $csvSheet.UsedRange.Copy() | Out-Null
-        $sheet.Paste()
-        $sheet.UsedRange.EntireColumn.AutoFit() | Out-Null
-        if ($excelApp.Version -lt 14) {
-            $csvbook.Application.CutCopyMode = $false
-        }
-        $csvBook.Close($false, $null, $null)
-        Remove-Item -Path $tempCsvName -Confirm:$false
-        $workbook.Sheets.Item(1).Select()
-        $workbook.SaveAs($Path)
-        $excelApp.DisplayAlerts = $originalAlerts
-        $excelApp.Quit()
-        Stop-Process -Name "Excel"
-    }
-}
-
-
-function Start-Rdsclient {
+Function Start-RdsClient {
     <#
 .SYNOPSIS
 	Starts Remote Desktop Client
@@ -129,27 +34,6 @@ function Start-Rdsclient {
 
 set-alias ts start-rdsclient
 set-alias rds start-rdsclient
-
-function get-website {
-    <#
-.SYNOPSIS
-	Launches browser to a web site.
-.DESCRIPTION
-	The get-website function launches a browser to a website
-.NOTES
-	Author:  Mark Curole
-.PARAMETER Site
-	Specifies the site to access.
-.EXAMPLE
-	PS> get-website "http://www.microsoft.com"
-#>
-    Param ([parameter(ValueFromPipeline = $true, Position = 1)] [string] $Site = "http://www.google.com/ig?hl=en" )
-
-    [diagnostics.process]::start($Site)
-
-}
-
-set-alias gws get-website
 
 function Clear-VIServers {  
     [CmdletBinding(SupportsShouldProcess = $true,  
@@ -263,28 +147,22 @@ function Convert-IISLogsToObject {
 #set-alias putty "C:\Program Files (x86)\putty\putty.exe"
 set-alias zip write-zip
 set-alias unzip expand-archive
-set-alias xn "C:\Program Files (x86)\XML Notepad 2007\XmlNotepad.exe"
 set-alias rh Resolve-DnsName
 
 
 
 #. "$psscriptroot\ipxls.ps1"
 
-Export-ModuleMember -function Export-Xls
-Export-ModuleMember -function start-rdsclient
-Export-ModuleMember -function get-website
+Export-ModuleMember -function Start-RdsClient
 Export-ModuleMember -Function Clear-VIServers
 Export-ModuleMember -function Switch-DockerEngine
 Export-ModuleMember -Function Test-IsAdmin
 Export-ModuleMember -Function Convert-IISLogsToObject
 
-Export-ModuleMember -alias gws
 Export-ModuleMember -alias ts
 Export-ModuleMember -alias rds
-#Export-ModuleMember -alias putty
 Export-ModuleMember -alias zip
 Export-ModuleMember -alias unzip
-Export-ModuleMember -alias xn
 Export-ModuleMember -alias rh
 Export-ModuleMember -Alias cvis
 Export-ModuleMember -Alias sde
